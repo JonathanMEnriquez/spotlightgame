86b2a7465155ccf5504f00f9c823fde0fd547321
Jonathan Enriquez <jenriquez@techempower.com>
2020-02-12 16:52:29 -0800

    added guesses to history entries

--- src/SingleRecord.js
+++ src/SingleRecord.js
@@ -4,6 +4,7 @@ import './SingleRecord.css';
 const SingleRecord = (props) => {
     const [displayGuesses, setDisplayGuesses] = useState(false);
     const { record, key } = props;
+    const guessesKeys = Object.keys(record.guesses);
 
     const toggleGuessDisplay = () => {
         setDisplayGuesses(displayGuesses => !displayGuesses);
@@ -11,21 +12,21 @@ const SingleRecord = (props) => {
 
     return (
     <tbody className="entry">
-    <tr key={key} className="record" onClick={toggleGuessDisplay}>
-        <td>{record.date}</td>
-        <td>{record.location}</td>
-        <td>{record.winner}</td>
-        <td><a target="_blank" rel="noopener noreferrer" href={record.imgSrc}>Link</a></td>
-    </tr>
-    <tr className={displayGuesses ? 'guesses' : 'hidden'}>
-    <div>
-        {Object.keys(record.guesses).map((k, i) => {
-            return (
-            <span key={i}>{`${k}: ${record.guesses[k]}`}</span>
-            )
-        })}
-    </div>
-    </tr>
+        <tr key={key} className="record" onClick={toggleGuessDisplay}>
+            <td>{record.date}</td>
+            <td>{record.location}</td>
+            <td>{record.winner}</td>
+            <td><a target="_blank" rel="noopener noreferrer" href={record.imgSrc}>Link</a></td>
+        </tr>
+        <tr className={displayGuesses ? 'guesses' : 'hidden'}>
+            <td colSpan={4}>
+                {guessesKeys.map((k, i) => {
+                    return (
+                    <span className="guess" key={i}>{`${k}: ${record.guesses[k]}${i === guessesKeys.length - 1 ? '' : ', '}`}</span>
+                    )
+                })}
+            </td>
+        </tr>
     </tbody>
     )
 }
