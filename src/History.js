c1324ea611f9e56bc35beef82864ce02529bdb32
user <user@users-MacBook-Pro.local>
2020-02-13 21:01:13 -0800

    initial commit

--- src/History.js
+++ src/History.js
@@ -9,23 +9,23 @@ const History = (props) => {
     <div className={shouldDisplay ? 'history' : 'hidden'}>
         <img className="close" src={Close} alt="x" onClick={hideHistory} />
         <h3>Previous Results</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Location</th>
-                    <th>Winner</th>
-                    <th>Source</th>
-                </tr>
-            </thead>
-            {/* <tbody> */}
-            {history && history.map((en, i) => {
-            return (
-                <SingleRecord key={i} record={en} />
-            )
-            })}
-            {/* </tbody> */}
-        </table>
+        <div className="results">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Location</th>
+                        <th>Winner</th>
+                        <th>Source</th>
+                    </tr>
+                </thead>
+                {history && history.map((en, i) => {
+                return (
+                    <SingleRecord key={i} record={en} />
+                )
+                })}
+            </table>
+        </div>
     </div>
     )
 }
